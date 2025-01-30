bf6637b2fcf8d0c87615210aed18b1391d3c1788
farooq-teqniqly <farooq@teqniqly.com>
2025-01-30 11:06:45 -0800

    chore: Converted UserComponent to use ngSrc.

--- todo/src/app/user/user.component.ts
+++ todo/src/app/user/user.component.ts
@@ -1,12 +1,13 @@
 import { Component } from "@angular/core";
 import { DUMMY_USERS } from "../dummy-users";
+import { NgOptimizedImage } from "@angular/common";
 
 const randomIndex = () => Math.floor(Math.random() * DUMMY_USERS.length);
 
 @Component({
   selector: "app-user",
   standalone: true,
-  imports: [],
+  imports: [NgOptimizedImage],
   templateUrl: "./user.component.html",
   styleUrl: "./user.component.css",
 })
