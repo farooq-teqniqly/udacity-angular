55cea2af77456e486d7ab37d55c3df4f8cc325ff
farooq-teqniqly <farooq@teqniqly.com>
2025-02-03 14:29:08 -0800

    chore: Add form binding and submission logic to NewTaskComponent.

--- todo/src/app/new-task/new-task.component.ts
+++ todo/src/app/new-task/new-task.component.ts
@@ -1,15 +1,27 @@
-import { Component, output } from "@angular/core";
+import { Component, output, signal } from "@angular/core";
+import { FormsModule } from "@angular/forms";
 
 @Component({
   selector: "app-new-task",
-  imports: [],
+  imports: [FormsModule],
   templateUrl: "./new-task.component.html",
   styleUrl: "./new-task.component.css",
 })
 export class NewTaskComponent {
   cancelNewTask = output();
+  enteredTitle = signal("");
+  enteredSummary = signal("");
+  enteredDate = signal("");
 
   onCancel() {
     this.cancelNewTask.emit();
+  }
+
+  onSubmit() {
+    console.log({
+      title: this.enteredTitle(),
+      summary: this.enteredSummary(),
+      dueDate: this.enteredDate(),
+    });
   }
 }
