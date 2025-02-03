cbd485fae4c97ecc6ef2fb995b05f17865f208c1
farooq-teqniqly <farooq@teqniqly.com>
2025-02-03 10:36:24 -0800

    refactor: Introduced a `User` interface and updated related logic to use the interface. Added task filtering by user in `TaskListComponent` and dynamically rendered tasks in the template.

--- todo/src/app/app.component.ts
+++ todo/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component, signal } from "@angular/core";
 import { HeaderComponent } from "./header/header.component";
 import { UserComponent } from "./user/user.component";
-import { DUMMY_USERS } from "./dummy-users";
+import { DUMMY_USERS, User } from "./dummy-users";
 import { TaskListComponent } from "./task-list/task-list.component";
 
 @Component({
@@ -12,10 +12,10 @@ import { TaskListComponent } from "./task-list/task-list.component";
 })
 export class AppComponent {
   users = DUMMY_USERS;
-  selectedUserName = signal<string>("");
+  selectedUser = signal<User | undefined>(undefined);
 
   onSelectUser(id: string) {
-    const userName = DUMMY_USERS.find((user) => user.id === id)?.name;
-    this.selectedUserName.set(userName ?? "");
+    const user = DUMMY_USERS.find((user) => user.id === id);
+    this.selectedUser.set(user ?? undefined);
   }
 }
--- todo/src/app/task-list/task-list.component.ts
+++ todo/src/app/task-list/task-list.component.ts
@@ -1,5 +1,13 @@
-import { Component, input } from "@angular/core";
+import { Component, computed, input } from "@angular/core";
 import { TaskComponent } from "../task/task.component";
+
+interface Task {
+  id: string;
+  userId: string;
+  title: string;
+  summary: string;
+  dueDate: Date;
+}
 
 @Component({
   selector: "app-task-list",
@@ -9,5 +17,41 @@ import { TaskComponent } from "../task/task.component";
   styleUrl: "./task-list.component.css",
 })
 export class TaskListComponent {
+  userId = input.required<string>();
   userName = input.required<string>();
+
+  selectedUserTasks = computed(() =>
+    this.tasks.filter((task) => task.userId === this.userId())
+  );
+
+  tasks: Task[] = [
+    {
+      id: "1",
+      userId: "u1",
+      title: "Task 1",
+      summary: "Task 1 summary",
+      dueDate: new Date("2025-02-10"),
+    },
+    {
+      id: "2",
+      userId: "u1",
+      title: "Task 2",
+      summary: "Task 2 summary",
+      dueDate: new Date("2025-02-06"),
+    },
+    {
+      id: "3",
+      userId: "u2",
+      title: "Task 3",
+      summary: "Task 3 summary",
+      dueDate: new Date("2025-02-06"),
+    },
+    {
+      id: "4",
+      userId: "u3",
+      title: "Task 4",
+      summary: "Task 4 summary",
+      dueDate: new Date("2025-02-05"),
+    },
+  ];
 }
