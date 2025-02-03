b035548a7f3d17c983cc0e99abd2432320a18d35
farooq-teqniqly <farooq@teqniqly.com>
2025-02-03 10:17:40 -0800

    chore: Added TaskComponent and integrate it into TaskListComponent.

--- todo/src/app/task-list/task-list.component.ts
+++ todo/src/app/task-list/task-list.component.ts
@@ -1,9 +1,10 @@
 import { Component, input } from "@angular/core";
+import { TaskComponent } from "../task/task.component";
 
 @Component({
   selector: "app-task-list",
   standalone: true,
-  imports: [],
+  imports: [TaskComponent],
   templateUrl: "./task-list.component.html",
   styleUrl: "./task-list.component.css",
 })
