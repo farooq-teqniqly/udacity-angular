ec467b1216db2da617075a6b8d67dc1d3d9fc44a
farooqfarooq@teqniqly.com <farooq@teqniqly.com>
2025-02-05 11:36:35 -0800

    chore: Format monetary values using currency pipe.

--- calculator/src/app/invest-projection-table/invest-projection-table.component.ts
+++ calculator/src/app/invest-projection-table/invest-projection-table.component.ts
@@ -1,9 +1,10 @@
 import { Component, input } from "@angular/core";
 import { InvestmentProjectionTableRow } from "./investment-projection-table-row";
+import { CurrencyPipe } from "@angular/common";
 
 @Component({
   selector: "app-invest-projection-table",
-  imports: [],
+  imports: [CurrencyPipe],
   templateUrl: "./invest-projection-table.component.html",
   styleUrl: "./invest-projection-table.component.css",
 })
