0ade8d291eb862779729665cf5a2ca32e7e450cb
farooqfarooq@teqniqly.com <farooq@teqniqly.com>
2025-02-05 10:39:08 -0800

    chore: Added input form for investment calculation.

--- calculator/src/app/calculator-form/calculator-form.component.ts
+++ calculator/src/app/calculator-form/calculator-form.component.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { FormsModule } from "@angular/forms";
 
 @Component({
-  selector: 'app-calculator-form',
-  imports: [],
-  templateUrl: './calculator-form.component.html',
-  styleUrl: './calculator-form.component.css'
+  selector: "app-calculator-form",
+  imports: [FormsModule],
+  templateUrl: "./calculator-form.component.html",
+  styleUrl: "./calculator-form.component.css",
 })
 export class CalculatorFormComponent {
+  initialInvestmentRange: [number, number] = [1000, 100000];
+  annualInvestmentRange: [number, number] = [100, 10000];
+  expectedReturnRange: [number, number] = [1, 20];
+  durationRange: [number, number] = [1, 20];
 
+  initialInvestment: number = this.initialInvestmentRange[0];
+  annualInvestment: number = this.annualInvestmentRange[0];
+  expectedReturn: number = this.expectedReturnRange[0];
+  duration: number = this.durationRange[0];
+
+  onCalculate() {}
 }
