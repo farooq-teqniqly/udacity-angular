5a4f66d05ab1d738d02150db4c7a0d7ed984a141
farooqfarooq@teqniqly.com <farooq@teqniqly.com>
2025-02-05 15:38:59 -0800

    refactor: Cleanup CalculatorFormComponent by removing validation because we don't need it.

--- calculator/src/app/calculator-form/calculator-form.component.ts
+++ calculator/src/app/calculator-form/calculator-form.component.ts
@@ -9,15 +9,10 @@ import { CalculationParameters } from "./calculation-parameters.model";
   styleUrl: "./calculator-form.component.css",
 })
 export class CalculatorFormComponent {
-  initialInvestmentRange: [number, number] = [1000, 100000];
-  annualInvestmentRange: [number, number] = [100, 10000];
-  expectedReturnRange: [number, number] = [1, 20];
-  durationRange: [number, number] = [1, 20];
-
-  initialInvestment: number = this.initialInvestmentRange[0];
-  annualInvestment: number = this.annualInvestmentRange[0];
-  expectedReturn: number = this.expectedReturnRange[0];
-  duration: number = this.durationRange[0];
+  initialInvestment = 1000;
+  annualInvestment = 1000;
+  expectedReturn = 5.5;
+  duration = 10;
 
   formData = output<CalculationParameters>();
 
